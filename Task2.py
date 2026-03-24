f04a07afeeac1048a49e0fb37e4439fd7024250a
Agam-B <s20869@caseygrammar.vic.edu.au>
2026-03-24 04:08:53 +0000

    Submit1

--- Task2.py
+++ Task2.py
@@ -1,8 +1,9 @@
 # Task 2: James Bond
 def main():
     #Write your code for input here
-    
-
+    first = input('What is your first name? ')
+    last = input('What is your last name? ')
+    print('My name is ' + last + ', ' + first + ' '+ last)
 
     # End of your code for input here
 
--- Task3.py
+++ Task3.py
@@ -3,8 +3,9 @@
 # Write a program that asks the user
 def main():
     #Write your code for input here
-    
-
+    dog = int(input('Dog years: '))
+    human = dog * 7
+    print(f'Human years = {human}')
 
     # End of your code for input here
 
